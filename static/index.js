10e99c4a9ba6e1ef2fb35bc0ec10629995dbfc43
Jenni M <jennifermoulder17@gmail.com>
2021-02-17 18:36:55 +0000

    Creating Canvas for drawing

--- static/index.js
+++ static/index.js
@@ -1,2 +1,52 @@
 alert("Hello, world!");
 console.log("HEY");
+
+// var canvas = document.getElementById("Canvas");
+// var context = canvas.getContext("2d");
+// context.fillStyle = "#FF0000";
+
+var mousePressed = false;
+var lastX, lastY;
+var ctx;
+
+function InitThis() {
+    ctx = document.getElementById('Canvas').getContext("2d");
+
+    $('#Canvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#Canvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#Canvas').mouseup(function (e) {
+        mousePressed = false;
+    });
+	    $('#Canvas').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = $("black").val();
+        ctx.lineWidth = $("3").val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+	
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
