e6c2a57082ed913ce3d9c817a9ae731dd97d9b47
RajjSinghh <gurrajsingh22@gmail.com>
2021-04-22 14:39:23 +0100

    Added NN guessing to server

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import cv2
+import matplotlib.pyplot as plt
 import flask
 import numpy as np
+import cairocffi as cairo
 import tensorflow as tf
 import sys
 from numpy import reshape
@@ -63,26 +65,19 @@ def solution(name):
 
 @app.route("/send/image", methods=['POST'])
 def send():
+    CATEGORIES = ["tooth", "hand", "tree", "sun", "rainbow", "alarm clock", "hockey stick", "bear", "eraser"]
     image = request.json
     image = image["pixels"]
-    print(image, file=sys.stderr)
-    with open("bugfix.json", "w") as file:
-        file.write(json.dumps(image))
-    
-    #image = image.reshape((1,28,28,1))
-    #img2 = image["pixels"]
-    model = tf.keras.models.load_model("NeuralNetworkModel")
-    try:
-        tensor = tf.convert_to_tensor(image, dtype=tf.int64)
-        guess = model.predict(tensor)
-        print("FIRST", file=sys.stderr)
-    except:
-        print("SECOND", file=sys.stderr)
-        guess = "I don't know!"
-    
-    return redirect(url_for('solution', name = guess))
+    image = vector_to_raster(image)
+    pred = np.argmax(model.predict(np.array([image])))
+    answer = CATEGORIES[pred]
+
+    print(answer)
+    return answer
+    #return redirect(url_for('solution',))
     
 if __name__ == "__main__":
-    model = tf.keras.models.load_model("NeuralNetworkModel")
-    #print(model)
-    #app.run(debug = True)
+    global model
+    model = tf.keras.models.load_model("saved_models/final.h5")
+    print("Server running...")
+    app.run()
